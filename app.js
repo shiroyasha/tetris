46da968592bca7eaba71d0c20e16dadfd590f08c
Igor Šarčević <igor@renderedtext.com>
2015-11-29 12:46:36 +0100

    Implement basic game loop

--- app.js
+++ app.js
@@ -2,10 +2,32 @@ class Game {
   constructor(canvas) {
     this.canvas = canvas;
     this.canvasContext = this.canvas.getContext("2d");
+    this.lastTick = null;
   }
 
   start() {
+    this.loop();
+  }
+
+  loop() {
+    window.requestAnimationFrame((timestamp) => {
+      this.step(timestamp);
+      this.loop();
+    });
+  }
+
+  step(timestamp) {
+    var deltaTime = timestamp - (this.lastTick || timestamp);
+
+    this.lastTick = timestamp;
+
+    this.update(deltaTime);
     this.render(this.canvasContext);
+
+  }
+
+  update(dt) {
+    //console.log(dt);
   }
 
   render(g) {
