91d646119de29661c761757470db1375f832c649
Igor Šarčević <igor@renderedtext.com>
2015-11-29 12:31:48 +0100

    Draw black box

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-function test() {
-  console.log("here")
+class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.canvasContext = this.canvas.getContext("2d");
+  }
+
+  start() {
+    this.canvasContext.fillRect(0, 0, 300, 300);
+  }
 }
+
+var canvas = document.getElementById("gameCanvas");
+
+var game = new Game(canvas);
+
+game.start();
